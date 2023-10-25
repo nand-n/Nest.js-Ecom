d4d2406a8c96c2a0071e4ed3f4f551ee0e71b631
Nahom <nahomdebele002@gmail.com>
2023-10-25 22:39:43 +0300

    added products get

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Delete, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private prodcutService: ProductService) {}
+
+  @Get()
+  getProducts(
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ) {
+    return this.prodcutService.create(name, price, description);
+  }
 
   @Post()
   createProduct(
